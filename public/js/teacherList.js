3400b79486a50de4f45f82ec2dc1dfb4a11913ee
Cao Guoqing <15311421687.com>
2017-04-15 13:56:05 +0800

    完成讲师的注销与启动功能

--- public/js/teacherList.js
+++ public/js/teacherList.js
@@ -10,6 +10,30 @@ define(['jquery','template'],function($,template){
                 //返回成功，渲染讲师数据列表
                 var html = template('teacherTpl',{list:data.result});
                 $('#teacherList').html(html);
+                //讲师注销/启用
+                $('.ecTeacher').click(function(){
+                    var td = $(this).parent('td');
+                    var tc_id = td.attr('data_id');
+                    var tc_status = td.attr('data_status');
+                    var that = this;
+                    $.ajax({
+                        type:'post',
+                        data:{tc_id:tc_id,tc_status:tc_status},
+                        url:'/api/teacher/handle',
+                        success:function(data){
+                            if(data.code == 200){
+                                //更新页面点击讲师的状态
+                                td.attr('data_status',data.result.tc_status);
+                                //修改讲师当前的状态内容
+                                if(data.result.tc_status == 0){
+                                    $(that).html('启用');
+                                }else{
+                                    $(that).html('注销');
+                                }
+                            }
+                        }
+                    });
+                });
             }
         }
     });
